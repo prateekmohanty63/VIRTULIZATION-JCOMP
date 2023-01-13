b3cda763982e69a13abf6fd4fd2d654840965d9a
Prateek <prateekmohanty63@gmail.com>
2023-01-13 23:49:15 +0530

    Communication working

--- appointmentsystem/consumer.py
+++ appointmentsystem/consumer.py
@@ -1,6 +1,7 @@
 
 
 import pika,json
+from appointment.models import TempDoc
 
 params=pika.URLParameters('amqps://lekpusth:E2YUqJHfhT78MLUBzsrivqxEAtYhCNVA@sparrow.rmq.cloudamqp.com/lekpusth')
 
@@ -14,9 +15,16 @@ channel.queue_declare(queue='jeevan_raksha_appointment_queue')
 
 
 def callback(ch,method,properties,body):
-    print('Recieved in Jeevan Raksha Admin')
+    print('Recieved in Jeevan Raksha Appointment App')
     data=json.loads(body)
     print(data)
+    print(properties.content_type)
+
+    if properties.content_type=='doctor_registered':
+        doctor=TempDoc.objects.create(Email=data['email'], Username=data['username'])
+        print('Doctor created')
+
+
 
 channel.basic_consume(queue='jeevan_raksha_appointment_queue',on_message_callback=callback,auto_ack=True)
 
